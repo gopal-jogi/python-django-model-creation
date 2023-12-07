511f6d4a87bcb1369554403543ddaec1ded4a07b
Gopal Dnyaneshwar Jogi <gopaljogi5@gmail.com>
2023-12-07 18:13:18 +0530

    Changes to be committed: 	modified:   app/__pycache__/models.cpython-311.pyc 	modified:   app/models.py 	modified:   db.sqlite3

--- app/models.py
+++ app/models.py
@@ -4,12 +4,21 @@ from django.db import models
 class Topic(models.Model):
     topic_name=models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.topic_name
+
 class WebPages(models.Model):
     topic_name=models.ForeignKey(Topic,on_delete=models.CASCADE)
     name=models.CharField(max_length=100)
     url=models.URLField()
 
+    def __str__(self):
+        return self.name
+
 class AccessRecord(models.Model):
     name=models.ForeignKey(WebPages,on_delete=models.CASCADE)
     date=models.DateField()
     author=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.author
